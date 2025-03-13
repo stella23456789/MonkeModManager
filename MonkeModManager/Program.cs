cf5d17753141c43dc8a57e0352094b4013179ce7
NgbatzYT <94680235+NgbatzYT@users.noreply.github.com>
2025-03-13 21:07:06 +1100

    Add files via upload

--- MonkeModManager/Program.cs
+++ MonkeModManager/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MonkeModManager
@@ -14,7 +16,7 @@ namespace MonkeModManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new Form1());
         }
     }
 }
